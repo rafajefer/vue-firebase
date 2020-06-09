79d46217692561a029bbf54d1eae5606d7876ffc
Rafael pena <rafael.pena@visie.com.br>
2020-06-09 10:33:53 -0300

    Criado o componente de login e register

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <img alt="Vue logo" src="./assets/logo.png" />
     <router-view/>
   </div>
 </template>
