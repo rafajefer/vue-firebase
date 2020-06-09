fbe2735d1050dcffc0ca356572090313a36245ea
Rafael pena <rafael.pena@visie.com.br>
2020-06-09 10:49:12 -0300

    Instalado o firebase e metodo de criar conta via email e senha

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzuYa90WN8OgJ1vQP7-avGQh5NRWuo1Fk",
+  authDomain: "expenses-46cdb.firebaseapp.com",
+  databaseURL: "https://expenses-46cdb.firebaseio.com",
+  projectId: "expenses-46cdb",
+  storageBucket: "expenses-46cdb.appspot.com",
+  messagingSenderId: "859884447605",
+  appId: "1:859884447605:web:92e7f85ba6ec547710953c",
+  measurementId: "G-LH3MRMBJ62"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
